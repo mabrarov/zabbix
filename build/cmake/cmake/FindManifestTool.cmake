8672eb17826a0034d79492b423a8186783ec1490
Marat Abrarov <abrarov@gmail.com>
2018-04-13 11:41:30 +0300

    Configurable search for Microsoft Message Compiler and Microsoft Manifest Tool.

--- build/cmake/cmake/FindManifestTool.cmake
+++ build/cmake/cmake/FindManifestTool.cmake
@@ -2,29 +2,65 @@
 
 find_package(PkgConfig QUIET)
 
-get_filename_component(win_sdk_dir "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows;CurrentInstallFolder]" REALPATH)
-if(CMAKE_C_SIZEOF_DATA_PTR EQUAL 8)
-    list(APPEND win_sdk_binary_suffixes "bin/x64" "bin")
-else()
-    list(APPEND win_sdk_binary_suffixes "bin/x86" "bin")
-endif()
+function(_manifest_tool_find)
+    if(WIN_SDK_ROOT)
+        list(APPEND win_sdk_roots "${WIN_SDK_ROOT}")
+    else()
+        set(win_sdk_root_found FALSE)
+        set(found_win_sdk_root )
+        if(NOT "$ENV{WIN_SDK_ROOT}" STREQUAL "")
+            # Fallback to WIN_SDK_ROOT enviornment variable
+            set(found_win_sdk_root "$ENV{WIN_SDK_ROOT}")
+            set(win_sdk_root_found TRUE)
+        else()
+            # Try to find location of the latest Windows SDK in Windows Registry
+            get_filename_component(found_win_sdk_root
+                "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows;CurrentInstallFolder]"
+                REALPATH)
+            if(NOT "${found_win_sdk_root}" STREQUAL "")
+                set(win_sdk_root_found TRUE)
+            endif()
+        endif()
+        if(win_sdk_root_found)
+            file(TO_CMAKE_PATH "${found_win_sdk_root}" cmake_path)
+            list(APPEND win_sdk_roots "${cmake_path}")
+            set(WIN_SDK_ROOT "${cmake_path}"
+                CACHE PATH "Location of the Windows SDK" FORCE)
+        endif()
+    endif()
 
-find_program(CMAKE_MANIFEST_TOOL "mt"
-    HINTS ${win_sdk_dir}
-    PATH_SUFFIXES ${win_sdk_binary_suffixes}
-    DOC "path to Microsoft Manifest Tool")
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        # 64-bit binary directory
+        set(_bit_bin "bin/x64")
+    else()
+        # 32-bit binary directory
+        set(_bit_bin "bin/x86")
+    endif()
 
-mark_as_advanced(CMAKE_MANIFEST_TOOL)
+    # Generic 64-bit and 32-bit directories
+    list(APPEND win_sdk_binary_suffixes "${_bit_bin}" "bin")
+
+    # Find executable
+    find_program(CMAKE_MANIFEST_TOOL "mt"
+        HINTS ${win_sdk_roots}
+        PATH_SUFFIXES ${win_sdk_binary_suffixes}
+        DOC "path to Microsoft Manifest Tool")
+    mark_as_advanced(CMAKE_MANIFEST_TOOL)
+    set(CMAKE_MANIFEST_TOOL "${CMAKE_MANIFEST_TOOL}" PARENT_SCOPE)
+endfunction()
+
+_manifest_tool_find()
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(ManifestTool
     FOUND_VAR ManifestTool_FOUND
     REQUIRED_VARS CMAKE_MANIFEST_TOOL
-    FAIL_MESSAGE "Failed to find Microsoft Manifest Tool")
+    FAIL_MESSAGE "Failed to find Microsoft Message Compiler")
 
 if(ManifestTool_DEBUG)
     message(STATUS "--------FindManifestTool.cmake results debug--------")
-    message(STATUS "Microsoft Manifest Tool found: ${ManifestTool_FOUND}")
-    message(STATUS "Microsoft Manifest Tool: ${CMAKE_MANIFEST_TOOL}")
+    message(STATUS "Microsoft Message Compiler found: ${ManifestTool_FOUND}")
+    message(STATUS "Microsoft Message Compiler: ${CMAKE_MANIFEST_TOOL}")
     message(STATUS "----------------")
 endif()
+
--- build/cmake/cmake/FindMessageCompiler.cmake
+++ build/cmake/cmake/FindMessageCompiler.cmake
@@ -2,19 +2,54 @@
 
 find_package(PkgConfig QUIET)
 
-get_filename_component(win_sdk_dir "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows;CurrentInstallFolder]" REALPATH)
-if(CMAKE_C_SIZEOF_DATA_PTR EQUAL 8)
-    list(APPEND win_sdk_binary_suffixes "bin/x64" "bin")
-else()
-    list(APPEND win_sdk_binary_suffixes "bin/x86" "bin")
-endif()
+function(_message_compiler_find)
+    if(WIN_SDK_ROOT)
+        list(APPEND win_sdk_roots "${WIN_SDK_ROOT}")
+    else()
+        set(win_sdk_root_found FALSE)
+        set(found_win_sdk_root )
+        if(NOT "$ENV{WIN_SDK_ROOT}" STREQUAL "")
+            # Fallback to WIN_SDK_ROOT enviornment variable
+            set(found_win_sdk_root "$ENV{WIN_SDK_ROOT}")
+            set(win_sdk_root_found TRUE)
+        else()
+            # Try to find location of the latest Windows SDK in Windows Registry
+            get_filename_component(found_win_sdk_root
+                "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Microsoft SDKs\\Windows;CurrentInstallFolder]"
+                REALPATH)
+            if(NOT "${found_win_sdk_root}" STREQUAL "")
+                set(win_sdk_root_found TRUE)
+            endif()
+        endif()
+        if(win_sdk_root_found)
+            file(TO_CMAKE_PATH "${found_win_sdk_root}" cmake_path)
+            list(APPEND win_sdk_roots "${cmake_path}")
+            set(WIN_SDK_ROOT "${cmake_path}"
+                CACHE PATH "Location of the Windows SDK" FORCE)
+        endif()
+    endif()
 
-find_program(CMAKE_MC_COMPILER "mc"
-    HINTS ${win_sdk_dir}
-    PATH_SUFFIXES ${win_sdk_binary_suffixes}
-    DOC "path to Microsoft Message Compiler")
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        # 64-bit binary directory
+        set(_bit_bin "bin/x64")
+    else()
+        # 32-bit binary directory
+        set(_bit_bin "bin/x86")
+    endif()
 
-mark_as_advanced(CMAKE_MC_COMPILER)
+    # Generic 64-bit and 32-bit directories
+    list(APPEND win_sdk_binary_suffixes "${_bit_bin}" "bin")
+
+    # Find executable
+    find_program(CMAKE_MC_COMPILER "mc"
+        HINTS ${win_sdk_roots}
+        PATH_SUFFIXES ${win_sdk_binary_suffixes}
+        DOC "path to Microsoft Message Compiler")
+    mark_as_advanced(CMAKE_MC_COMPILER)
+    set(CMAKE_MC_COMPILER "${CMAKE_MC_COMPILER}" PARENT_SCOPE)
+endfunction()
+
+_message_compiler_find()
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(MessageCompiler
